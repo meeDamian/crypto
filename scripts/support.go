fd3ade9cff5d985217f6089463f5e3c4b89d9e1d
Damian Mee <spam@meedamian.com>
2018-05-30 16:46:56 +0700

    .AllOrderBooks() added to stats script

--- scripts/support.go
+++ scripts/support.go
@@ -8,15 +8,15 @@ import (
 )
 
 func main() {
-	header := fmt.Sprintf("| %-15s | %-10s | %-12s | %-10s", "Domain", ".Markets()", ".OrderBook()", ".Balances()")
+	header := fmt.Sprintf("| %-15s | %-10s | %-12s | %-17s | %-10s", "Domain", ".Markets()", ".OrderBook()", ".AllOrderBooks()",".Balances()")
 	length := len(header)
 
 	fmt.Println(header)
 	fmt.Println(strings.Repeat("-", length+1))
 
-	var markets, obs, balances int64
+	var markets, obs, aobs, balances int64
 	for _, e := range exchanges.All {
-		fmt.Printf("| %-15s | %-10t | %-12t | %t\n", e.Domain, e.Markets != nil, e.OrderBook != nil, e.Balances != nil)
+		fmt.Printf("| %-15s | %-10t | %-12t | %-17t | %t\n", e.Domain, e.Markets != nil, e.OrderBook != nil, e.AllOrderBooks!=nil, e.Balances != nil)
 
 		if e.Markets != nil {
 			markets++
@@ -26,11 +26,15 @@ func main() {
 			obs++
 		}
 
+		if e.AllOrderBooks != nil {
+			aobs++
+		}
+
 		if e.Balances != nil {
 			balances++
 		}
 	}
 
 	fmt.Println(strings.Repeat("-", length+1))
-	fmt.Printf("| %15s | %-10d | %-12d | %d", "done:", markets, obs, balances)
+	fmt.Printf("| %15s | %-10d | %-12d | %-17d | %d", "done:", markets, obs, aobs, balances)
 }
