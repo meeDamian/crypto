3109a47500059810adfc7ca29d13dc6233c1d139
Damian Mee <spam@meedamian.com>
2018-06-06 00:49:21 +0700

    add missing ability for users to set their missing currency trigger

--- market.go
+++ market.go
@@ -2,6 +2,7 @@ package crypto
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/meeDamian/crypto/currencies"
 	"github.com/pkg/errors"
@@ -15,15 +16,47 @@ func (m Market) String() string {
 	return fmt.Sprintf("%s/%s", m.Asset, m.PricedIn)
 }
 
-func NewMarket(asset, price string) (m Market, err error) {
+var currencyNotSupportedCustomTrigger = func(string, ...error) {} // noop by default
+
+func SetCurrencyNotSupportedTrigger(userFn func(string, ...error)) {
+	if userFn == nil {
+		return
+	}
+
+	currencyNotSupportedCustomTrigger = userFn
+}
+
+func splitSymbol(symbol string, fn func(string, ...error)) {
+	codes := strings.Split(symbol, "_")
+	if len(codes) > 1 {
+		NewMarket(codes[0], codes[1])
+		return
+	}
+
+	codes = strings.Split(symbol, "/")
+	if len(codes) > 1 {
+		NewMarket(codes[0], codes[1])
+		return
+	}
+
+	fn("", errors.Errorf("Unable to split %s into currencies", symbol))
+}
+
+func NewMarket(asset, price string) (m Market, error error) {
 	a, err := currencies.Get(asset)
 	if err != nil {
-		return m, err
+		currencyNotSupportedCustomTrigger(asset)
+		error = err
 	}
 
 	p, err := currencies.Get(price)
 	if err != nil {
-		return m, err
+		currencyNotSupportedCustomTrigger(price)
+		error = err
+	}
+
+	if error != nil {
+		return m, error
 	}
 
 	return Market{a.Name, p.Name}, nil
@@ -31,26 +64,19 @@ func NewMarket(asset, price string) (m Market, err error) {
 
 // appends market only if both currencies are known
 func AppendMarket(markets []Market, rawAsset, rawPrice string) ([]Market, error) {
-	asset, err := currencies.Get(rawAsset)
+	market, err := NewMarket(rawAsset, rawPrice)
 	if err != nil {
 		return markets, err
 	}
 
-	price, err := currencies.Get(rawPrice)
-	if err != nil {
-		return markets, err
-	}
-
-	return append(markets, Market{
-		asset.Name,
-		price.Name,
-	}), nil
+	return append(markets, market), nil
 }
 
 // extracts a valid market from a given symbol, or returns an error
 func NewMarketFromSymbol(symbol string) (market Market, err error) {
 	matches := pairRegExp.FindAllStringSubmatch(symbol, -1)
 	if len(matches) == 0 {
+		splitSymbol(symbol, currencyNotSupportedCustomTrigger)
 		return market, errors.Errorf("symbol %s is invalid or contains unknown currency", symbol)
 	}
 
